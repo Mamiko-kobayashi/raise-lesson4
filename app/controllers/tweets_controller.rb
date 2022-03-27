feaa302c43351899c2c73dfaac7ac5854376d803
mamiko kobayashi <masic.mico1@gmail.com>
2022-03-27 13:42:32 +0000

    update

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,6 +5,13 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new # 新規投稿用の空のインスタンス
   end
+  
+# ============追加================
+    @tweet = Tweet.new(tweet_params)  # フォームから送られてきたデータ(body)をストロングパラメータを経由して@tweetに代入
+    @tweet.user_id = current_user.id # user_idの情報はフォームからはきていないので、deviseのメソッドを使って「ログインしている自分のid」を代入
+    @tweet.save
+    redirect_to tweets_path
+# =================================
 
   def index
     @tweets = Tweet.all
@@ -13,6 +20,13 @@ class TweetsController < ApplicationController
   def show
     @tweet = Tweet.find(params[:id])
   end
+  
+# ===============追加=============
+  private
+    def tweet_params
+      params.require(:tweet).permit(:body) # tweetモデルのカラムのみを許可
+    end
+# =================================
 
   def create
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+# ===============追加==============
+    @tweets = @user.tweets
+# ================================
   end
 end
